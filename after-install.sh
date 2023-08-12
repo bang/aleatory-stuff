c7263df4a1bb808532615090ccc9841f8b266736
Andre Carneiro <andregarciacarneiro@gmail.com>
2023-08-12 11:29:14 -0300

    - [ADD] .gitignore - [FIX] after-install.sh - Fixed docker and ohmyzsh install - [ADD] docker/stop-all - [ADD] nas-utilities - saving NAS icon because it's cool

--- after-install.sh
+++ after-install.sh
@@ -1,20 +1,28 @@
 
 ## installing essential and useful packages
+echo "Installing essential modules"
 sudo apt install -y vim vim-gtk3 tmux \
 git \
 build-essential \
 vlc \
 nmap \
-nfs-common
+nfs-common \
+neofetch \
+watchdog 
+echo "Done!"
 
+echo "Removing not-want shit!"
 # Removing don't-ask-for-install packages from Linux
 # * is not working on zsh, I don't know why!
 /bin/bash -c "sudo apt purge -y thunderbird* libreoffice*"
+echo "Done!"
 
-
+echo "Installing docker"
 ## installing docker according to https://docs.docker.com/engine/install/ubuntu/
 # removing possible already installed docker package
 sudo apt-get remove docker docker-engine docker.io containerd runc
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
 
 ## installing docker dependecies
 sudo apt-get update
@@ -23,40 +31,50 @@ sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common \
+    gnupg
+
+# Install keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
 
 # adding repos, keys etc
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# ubuntu release is passed as argument - TODO improve argument analysis
-ubuntu_release=$1
-# if argument is not passed, assuming xenial. This will break some day
-if [ -z "$ubuntu_release" ]; then
-    ubuntu_release="xenial"
-fi
 
-# adding docker official repository for Ubuntu
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $ubuntu_release \
-   stable"
+# Set up repository
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
 
 # finally installing docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# adding user to docker group
+sudo usermod -a -G docker $USER
+
+echo "Done!"
+
+echo "Installing Brave browser"
 # Installing brave browser
 sudo apt installing -y apt-transport-https curl gnupg
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -y brave-browser
+sudo apt update && sudo apt install -y brave-browser
 
-# installing oh my zsh
-sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Installing ohmyzsh"
+# Unset ZSH
+unset ZSH
+# Removing .oh-my-zsh directory
+rm -rf ~/.oh-my-zsh
+# To remember
+ZSH_THEME=refined
+# installing ohmyzsh
+sudo apt install -y zsh && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # installing extra fonts for oh my zsh themes
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
@@ -65,7 +83,10 @@ chmod +x install.sh
 cd ..
 rm -rf fonts
 
-# Installing design/streaming programs
-sudo apt install -y gimp inkscape obs-studio
+echo "Done!"
 
+# Installing design/streaming programs
+echo "Installing design/streaming programs"
+sudo apt install -y gimp inkscape obs-studio blender
+echo "Done!
 
